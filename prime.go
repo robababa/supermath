505ad388a791f834cfd06c0b27fc0494596ee80e
Rob Johnson <robj@hightouchinc.com>
2017-08-12 14:03:58 -0500

    Make prime list generator WAY more efficient

--- prime.go
+++ prime.go
@@ -1,13 +1,21 @@
 package supermath
 
+import "math"
+
 func PrimeSlice(maxint int) []int {
 	var answer []int
 	// loop through prime candidates
 CandidateLoop:
 	for i := 2; i <= maxint; i++ {
 		// see if any existing primes divide it
+		maxPossiblePrimeDivisor := int(math.Sqrt(float64(i)))
 		for _, prime := range answer {
 			if i%prime == 0 {
+				// i is composite, go to the next i
+				continue CandidateLoop
+			} else if prime > maxPossiblePrimeDivisor {
+				// we've already tested all primes less than or equal to sqrt(i), so i is prime
+				answer = append(answer, i)
 				continue CandidateLoop
 			}
 		}
